e403f73919a274da63bda71560ee538070a00e94
martina1612 <martina.ressegotti01@universitadipavia.it>
2018-12-18 11:29:39 +0100

    RecoLocalMuon/GEMRecHit and BuildFile.xml first commit
    RecoLocalMuon/GEMRecHit and BuildFile.xml first commit

---
+++ RecoLocalMuon/GEMRecHit/python/runME0localReco_cfg.py
@@ -0,0 +1,87 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("GEMLocalRECOSEGMENT")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20))
+
+#process.Timing = cms.Service("Timing")
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2023D20Reco_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.load('RecoLocalMuon.GEMRecHit.gemRecHits_cfi')
+#process.load('RecoLocalMuon.GEMRecHit.me0RecHits_cfi')
+process.load('RecoLocalMuon.GEMRecHit.me0LocalReco_cff')
+
+from RecoLocalMuon.Configuration.RecoLocalMuon_cff import *
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+process.load("CalibMuon.CSCCalibration.CSCChannelMapper_cfi")
+process.load("CalibMuon.CSCCalibration.CSCIndexer_cfi")
+process.CSCIndexerESProducer.AlgoName = cms.string("CSCIndexerPostls1")
+process.CSCChannelMapperESProducer.AlgoName = cms.string("CSCChannelMapperPostls1")
+process.CSCGeometryESModule.useGangedStripsInME1a = False
+process.csc2DRecHits.readBadChannels = cms.bool(False)
+process.csc2DRecHits.CSCUseGasGainCorrections = cms.bool(False)
+
+process.me0RecHits = cms.EDProducer("ME0RecHitProducer",
+    recAlgoConfig = cms.PSet(),
+    recAlgo = cms.string('ME0RecHitStandardAlgo'),
+    me0DigiLabel = cms.InputTag("simMuonME0Digis", "", "DIGI"), # 20022018
+    # maskSource = cms.string('File'),
+    # maskvecfile = cms.FileInPath('RecoLocalMuon/GEMRecHit/data/GEMMaskVec.dat'),
+    # deadSource = cms.string('File'),
+    # deadvecfile = cms.FileInPath('RecoLocalMuon/GEMRecHit/data/GEMDeadVec.dat')
+)
+
+### Input and Output Files
+##########################
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+	'file:/afs/cern.ch/work/m/mressego/private/ME0trigger/CMSSW_9_3_3/src/RecoLocalMuon/GEMRecHit/test/step2.root'
+    )
+)
+
+process.output = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string( 
+        'file:out_ME0localreco.root'
+    ),
+    outputCommands = cms.untracked.vstring(
+        'keep  *_*_*_*',
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('rechit_step')
+    )
+)
+
+### Paths and Schedules
+#######################
+process.rechit_step  = cms.Path(process.me0RecHits+process.me0LocalReco)
+#process.me0RecHits --> produces "me0RecHits" and "GEMLocalRECOSEGMENT"
+#process.me0LocalReco --> produces me0Segments"
+process.endjob_step  = cms.Path(process.endOfProcess)
+process.out_step     = cms.EndPath(process.output)
+
+
+process.schedule = cms.Schedule(
+    process.rechit_step,
+    process.endjob_step,
+    process.out_step
+)
+
